cfb3166ddd0ae45fe2a0384736148803512bf206
mattloose <matt.loose@nottingham.ac.uk>
2015-02-26 21:07:05 +0000

    Cleanup

--- mT_web/minoTour/includes/currentpills.php
+++
@@ -1,7 +0,0 @@
-<ul class="nav nav-pills">
-			  <li class="active"><a href="current_summary.php">Read Summaries</a></li>
-			  <li><a href="current_rates.php">Sequencing Rates</a></li>
-			  <li><a href="current_pores.php">Pore Activity</a></li>
-  			  <li><a href="current_quality.php">Read Quality</a></li> 
-			  <li><a href="current_coverage.php">Coverage Detail</a></li>
-</ul>
