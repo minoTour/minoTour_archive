13e587aaa62b1ff10b83194ddeef7402f2574a3f
Matt Loose <matt.loose@nottingham.ac.uk>
2015-08-12 12:09:54 +0100

    more minor bug fixes

--- mT_web/minoTour/jsonencode/coverage.php
+++ mT_web/minoTour/jsonencode/coverage.php
@@ -1,6 +1,14 @@
+
+
 <?php
 
 header('Content-Type: application/json');
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
     exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
