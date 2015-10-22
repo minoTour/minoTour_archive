8c6c68bb29c2c498fc7ca193a81c04669dfca6d7
Matt Loose <matt.loose@nottingham.ac.uk>
2015-10-22 21:44:00 +0100

    Fixes for cache reset

--- mT_web/minoTour/views/cache_manage.php
+++ mT_web/minoTour/views/cache_manage.php
@@ -13,14 +13,15 @@ require_once("includes/functions.php");
 <html>
 
 <?php include "includes/head.php";?>
-<meta http-equiv="refresh" content="10" > 
+
+<meta http-equiv="refresh" content="10" >
 
 <body>
 
     <div id="wrapper">
 
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
-            
+
             <!-- /.navbar-header -->
 			<?php include 'navbar-header.php' ?>
             <!-- /.navbar-top-links -->
@@ -39,13 +40,13 @@ require_once("includes/functions.php");
             <div class="row">
                 <div class="col-lg-12">
 				<h4>memcache</h4>
-				
+
 				<br>
-			 	
+
 				<br>
-				
+
 				minoTour can take advantage of memcache to speed up performace for all users. This page is used to test if memcache is running and - if so - provide you with some performance related stats.<br><br>
-				<?php 
+				<?php
 				$memcache->set("php_mem_cache_test", "test", 0, 10);
 				$cachecheck = $memcache->get("php_mem_cache_test");
 				if ($cachecheck === false) {
@@ -53,7 +54,7 @@ require_once("includes/functions.php");
 				}else{
 					echo "<strong>This minotour installation is using memcache.<br></strong>";
 				}
-				
+
 				$cachecheck = $memcache->get("perl_mem_cache_connection");
 				if($cachecheck === false){
 					echo "<strong>Your minotour installation is not using background perl scripts which accelerate web performance - please consult with the user manual to set these up.<br></strong>";
@@ -71,9 +72,9 @@ require_once("includes/functions.php");
 							$jsonreads = $runname . "bases";
 							$json_test = $memcache->get($jsonreads);
 							echo $jsonreads . "<br>";
-							echo "$json_test<br>";							
+							echo "$json_test<br>";
 						}
-						
+
 					}
 				}
 				?>
@@ -81,16 +82,34 @@ require_once("includes/functions.php");
 					echo '<pre>';
 				var_dump($_SESSION);
 				echo '</pre>';
+
+                echo '<pre>';
+                //echo "Hello";
+                $list = array();
+    $allSlabs = $memcache->getExtendedStats('slabs');
+    $items = $memcache->getExtendedStats('items');
+    foreach($allSlabs as $server => $slabs) {
+        foreach($slabs AS $slabId => $slabMeta) {
+            $cdump = $memcache->getExtendedStats('cachedump',(int)$slabId);
+            foreach($cdump AS $keys => $arrVal) {
+                if (!is_array($arrVal)) continue;
+                foreach($arrVal AS $k => $v) {
+                    echo $k .'<br>';
+                }
+            }
+        }
+    }
+   echo '</pre>';
 				?>
-				
+
             </div>
         </div>
         <!-- /#page-wrapper -->
 
     </div>
     <!-- /#wrapper -->
-	
-	
+
+
     <!-- Core Scripts - Include with every page -->
     <script src="js/jquery-1.10.2.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -101,18 +120,18 @@ require_once("includes/functions.php");
 			    <script type="text/javascript">
 				PNotify.prototype.options.styling = "fontawesome";
 				</script>
-  
-	
+
+
 	<!-- Highcharts Addition -->
 	<script src="js/highcharts.js"></script>
 	<script type="text/javascript" src="js/themes/grid-light.js"></script>
 	<script src="http://code.highcharts.com/4.0.3/modules/heatmap.js"></script>
 	<script src="http://code.highcharts.com/modules/exporting.js"></script>
-	
-	
 
-	
-	
+
+
+
+
     <!-- SB Admin Scripts - Include with every page -->
     <script src="js/sb-admin.js"></script>
 
