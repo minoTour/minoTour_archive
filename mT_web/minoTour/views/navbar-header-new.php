04647f176beb53bab0fd3f7f26866c9abc935ccb
Matt Loose <matt.loose@nottingham.ac.uk>
2017-01-29 21:36:55 +0000

    header fix

--- mT_web/minoTour/views/navbar-header-new.php
+++ mT_web/minoTour/views/navbar-header-new.php
@@ -41,6 +41,7 @@
             </span>
           </a>
           <ul class="dropdown-menu">
+
             <li class="header">You have {{livenumruns}} live runs</li>
             <li>
               <!-- inner menu: contains the messages -->
@@ -79,9 +80,9 @@
               <!-- Inner Menu: contains the notifications -->
               <ul class="menu">
                 <li><!-- start notification -->
-                  <a href="#">
+                  <!--<a href="#">
                     <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                  </a>
+                </a>-->
                 </li><!-- end notification -->
               </ul>
             </li>
@@ -101,20 +102,16 @@
               <!-- Inner menu: contains the tasks -->
               <ul class="menu">
                 <li><!-- Task item -->
+                    <!--
                   <a href="#">
                     <!-- Task title and progress text -->
-                    <h3>
+                    <!--<h3>
                       Design some buttons
                       <small class="pull-right">20%</small>
-                    </h3>
+                  </h3> -->
                     <!-- The progress bar -->
-                    <div class="progress xs">
-                      <!-- Change the css width attribute to simulate progress -->
-                      <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                        <span class="sr-only">20% Complete</span>
-                      </div>
-                    </div>
-                  </a>
+
+                  <!--</a>-->
                 </li><!-- end task item -->
               </ul>
             </li>
